b921cbdda83832f62688f5955b4d1007d0ecf302
gauravsinha <gauravsinha@192.168.29.161>
2020-10-21 07:29:11 +0530

    commit project to github

---
+++ E2EJenkins/src/test/java/com/maven/E2EJenkins/TestCase003.java
@@ -0,0 +1,22 @@
+package com.maven.E2EJenkins;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TestCase003 {
+@Test
+	
+	public void test()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/ChromeDriver");
+
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.w3schools.com/html/html_tables.asp");
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+	}
+
+}
